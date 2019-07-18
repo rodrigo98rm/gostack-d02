a5fe716bc68890d6dcd24caef758bfc7a6abe530
rodrigo98rm <mayerrodrigo98@gmail.com>
2019-07-18 08:25:53 -0300

    Update user

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -32,9 +32,61 @@ class UserController {
     return res.json({ id, name, email });
   }
 
-  // TODO: UPDATE
+  // UPDATE
   async update(req, res) {
-    return null;
+    // Request body validation
+    const schema = Joi.object({
+      name: Joi.string().required(),
+      email: Joi.string().email(),
+      password: Joi.string().min(6),
+      confirmPassword: Joi.string().when('password', {
+        is: Joi.string().required(),
+        then: Joi.string()
+          .required()
+          .valid(Joi.ref('password')),
+      }),
+      oldPassword: Joi.string().when('password', {
+        is: Joi.string().required(),
+        then: Joi.string().required(),
+      }),
+    });
+
+    const { error } = schema.validate(req.body);
+
+    if (error) {
+      // Return error message provided by Joi
+      return res.status(400).json({ error: error.details[0].message });
+    }
+
+    // Get user by id
+    const user = await User.findByPk(req.userId);
+
+    const { password, oldPassword } = req.body;
+    const newEmail = req.body.email;
+
+    // If email is being changed, it has to be unique
+    if (newEmail) {
+      if (newEmail !== user.email) {
+        const emailExists = await User.findOne({ where: { email: newEmail } });
+        if (emailExists) {
+          return res.status(400).json({ error: 'This email is unavailable' });
+        }
+      }
+    }
+
+    // If password id being changed, check if oldPassword is correct
+    if (password && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    // Update user
+    const { id, name, email } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+    });
   }
 }
 
